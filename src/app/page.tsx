277c085f4f9c65efb0f522a7fee363cc06e30c19
NadyaCodes <nadyacorscadden@gmail.com>
2024-05-27 20:43:15 -0400

    add all basic resumes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,68 @@ export default function HomePage() {
           >
             <h3 className="text-2xl font-bold">Administration</h3>
           </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-BA-Resume-2024.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Brand Ambassador</h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-CLCS.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Cleaner / Customer Service</h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-CS.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Customer Service</h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-CSAD.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">
+              Customer Service / Administration
+            </h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-DF.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Digital Freelancer</h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-DFAD.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">
+              Digital Freelancer / Administration
+            </h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-WD.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Web Developer</h3>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="Nadya-Corscadden-Resume-May-2024-WDDF.pdf"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">
+              Web Developer / Digital Freelancer
+            </h3>
+          </Link>
         </div>
       </div>
     </main>
